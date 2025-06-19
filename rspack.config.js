580dc2f2ac71f94e91650572b75644dc56577e36
Kei Sakamoto <thiry27015@gmail.com>
2025-06-19 18:11:54 +0900

    add repro

--- rspack.config.js
+++ rspack.config.js
@@ -2,6 +2,8 @@ const {defineConfig} = require("@rspack/cli");
 const {rspack} = require("@rspack/core");
 
 module.exports = defineConfig({
+  target: "web",
+  stats: "verbose",
   mode: "development",
   entry: "./src/index.js",
   output: {
@@ -17,14 +19,21 @@ module.exports = defineConfig({
           },
           {
             loader: "css-loader",
+            options: {
+              modules: {
+                exportLocalsConvention: "camelCase",
+              },
+            },
           },
         ],
       },
       {
         test: /\.js$/,
+        // exclude: /core-js/,
         use: {
           loader: "builtin:swc-loader",
           options: {
+            isModule: "unknown",
             jsc: {
               parser: {
                 syntax: "ecmascript",
@@ -33,7 +42,17 @@ module.exports = defineConfig({
             env: {
               mode: process.env.ENV_MODE,
               coreJs: require("core-js/package.json").version,
-              targets: ["IE 11"],
+              // Chrome 121 + not exclude core-js:
+              // ERROR in ./src/style.css
+              //   × Module build failed:
+              //   ╰─▶   × $ is not a function
+              //
+              // Chrome 121 + exclude core-js:
+              // build successful.
+              //
+              // Chrome 122 and later:
+              // build successful.
+              targets: ["Chrome 121"],
             },
           },
         },
